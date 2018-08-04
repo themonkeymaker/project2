9c21871d78a413b42e890f31281a6ca740e0341f
Maseeh Es-Haq <eshaqmaseeh@yahoo.com>
2018-08-04 19:39:08 -0400

    changes for heroku deploy

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost/donationcart");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/donationcart");
 
 mongoose.Promise = Promise;
 
