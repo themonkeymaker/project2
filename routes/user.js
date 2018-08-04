5d136e96645495bef7e2007ddf1b701436729360
Maseeh Es-Haq <eshaqmaseeh@yahoo.com>
2018-08-04 18:46:22 -0400

    clean up unused code and ignore node_modules

--- routes/user.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const userController = require("../controllers/user.js");
-
-router.get("/new", userController.new);
-router.get("/:id", userController.show);
-router.post("/", userController.create);
-
-module.exports = router;
